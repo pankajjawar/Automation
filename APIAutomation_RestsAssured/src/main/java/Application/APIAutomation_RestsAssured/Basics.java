c6719a8b154269dcafe8bff26b264598091c4d5f
pankajjawar <pankajjawar1993@gmail.com>
2025-01-02 20:14:19 +0800

    Api automation using Restassured

---
+++ APIAutomation_RestsAssured/src/main/java/Application/APIAutomation_RestsAssured/Basics.java
@@ -0,0 +1,16 @@
+package Application.APIAutomation_RestsAssured;
+
+public class Basics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//given - all input details 
+		//when - Submit the API -resource,http method
+		//Then - validate the response
+		//RestAssured.baseURI= "https://rahulshettyacademy.com";
+		
+
+	}
+
+}
